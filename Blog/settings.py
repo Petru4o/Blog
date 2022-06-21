1ffaeb87e224ff3a389edeca0c5664754a553643
Petru4o <saliypetro22@gmail.com>
2022-06-21 12:19:36 +0300

    Migrate database to PostgreSQL

--- Blog/settings.py
+++ Blog/settings.py
@@ -72,8 +72,14 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'simpleblog',
+        'USER': 'postgres',
+        'PASSWORD': 'saliy123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
